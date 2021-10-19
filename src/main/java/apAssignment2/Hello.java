1ec7b7e7d1d978ce3322c3aa9e1d21af1ac100c4
Shoaib Asghar <i190406@nu.edu.pk>
2021-10-19 13:43:31 +0500

    last time

--- src/main/java/apAssignment2/Hello.java
+++ src/main/java/apAssignment2/Hello.java
@@ -2,9 +2,10 @@ package apAssignment2;
 
 public class Hello {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-
+        System.out.println("lets coding");
 	}
 
 }
